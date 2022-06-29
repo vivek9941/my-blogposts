81afc726b5564be4cd36926404f2a1734191314a
Vivek Parmar <200040094@iitb.ac.in>
2022-06-30 01:30:15 +0530

    initial commit

---
+++ mb-env/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.text[:50]
